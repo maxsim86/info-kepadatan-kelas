edda0533e0be48ded6593797cea00a79e773abc8
Nor sairi bin ahmad shahabudin <maxsim86@gmail.com>
2024-10-29 08:47:49 +0800

    authenticated login system

--- config/settings.py
+++ config/settings.py
@@ -136,8 +136,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/kuiz"
+LOGOUT_REDIRECT_URL = "/kuiz"
 
 MEDIA_URL = "/media/"
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
