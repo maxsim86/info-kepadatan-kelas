473b12431b19cf531211a0dfcad2f115d67bac82
Nor sairi bin ahmad shahabudin <maxsim86@gmail.com>
2025-02-18 16:40:27 +0800

    error - await outside func

--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 from django.contrib.auth.models import User
 
-# class Conversation(models.Model):
-#     participants = models.ManyToManyField(User, related_name='conversation')
-#     created_add = models.DateTimeField(auto_now_add=True)
+class Conversation(models.Model):
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='conversation')
+    created_add = models.DateTimeField(auto_now_add=True)
 
-# class Message(models.Model):
-#     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
-#     content = models.TextField()
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
